8dc037535934332f711190cc9a0bd5e9fe37ef32
HrithikHarsha <ghharsha16@gmail.com>
2023-10-24 12:07:01 -0400

    help-button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
+import React from "react";
 import "../styles/Header.css";
+import logo from "../assets/header.png";
 
 export default function Header({ children, onQuit }) {
   return (
     <header className="header">
       <h1>
         <button onClick={onQuit} className="logo-button">
-          
-          <span>
-            <span className="poke">Memory</span>
-            <span className="memo">Grid</span>
-          </span>
+        <span className="poke">Memory</span>
+<img className="logo" src={logo} alt="memory" />
+<span className="memo">Tiles</span>
+
         </button>
       </h1>
       {children}
